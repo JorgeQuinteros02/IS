bfd9d5f4e66c6a4035921a381530fa296500f44f
Jorge Quinteros <jquinteros25@wooster.edu>
2025-05-01 22:44:23 -0400

    added allp4mcnn to reproduce experiments from Cohen and Welling 2016

---
+++ tfcode/allp4mcnn.py
@@ -0,0 +1,27 @@
+from keras import layers, Model
+from keras_gcnn.layers import GConv2D
+
+
+def make_p4mcnn():
+    inputs = layers.Input((32, 32, 3))
+
+    out_channels = [32, 32, 32, 64, 64, 64, 64, 64, 10]
+    ksizes = [3, 3, 3, 3, 3, 3, 3, 1, 1]
+    strides = [1, 1, 2, 1, 1, 2, 1, 1, 1]
+
+    x = layers.Dropout(0.2)(inputs)
+    h_in = 'Z2'
+    for out_c, ksize, stride in zip(out_channels, ksizes, strides):
+        padding = "valid" if (ksize == 1) else "same"
+        x = GConv2D(out_c, ksize, h_in, 'D4', stride, padding=padding)(x)
+        h_in = 'D4'
+        x = layers.Activation('relu')(x)
+
+        if stride == 2:
+            x = layers.Dropout(0.5)(x)
+
+    x = layers.Reshape((8, 8, 8, 10))(x)
+    x = layers.GlobalAveragePooling3D()(x)
+
+    allp4mcnn = Model(inputs=inputs, outputs=x)
+    return allp4mcnn
